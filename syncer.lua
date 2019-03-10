dbaa634e445ebd358bee5dcb5b60a3f1364a9599
Alija Bobija <bobija.alija@gmail.com>
2019-03-10 13:11:33 +0100

    Sync error codes
    Sent corresponding error code to syncer_fire_after_sync function.
    
    Only result code 200, returned from web services, will be considered as valid response.

--- syncer.lua
+++ syncer.lua
@@ -1,3 +1,10 @@
+NameCheapDynDnsSyncer = {
+    DNS_RECORD_UPDATED  = 1,
+    PUBLIC_IP_NO_CHANGE = 2,
+    PUBLIC_IP_SERVICE_HTTP_REQ_ERROR = 3,
+    DNR_RECORD_UPDATE_SERVICE_HTTP_REQ_ERROR = 4
+}
+
 local get_public_ip = function(callback)
     http.get('http://api.ipify.org', callback)
 end
@@ -10,9 +17,11 @@ local update_dyn_dns_record = function(options, callback)
         .. '&ip='       .. options.ip, callback)
 end
 
-local syncer_fire_after_sync = function(syncer, _tmr)
+local syncer_fire_after_sync = function(syncer, result, _tmr)
+    print('[DynDnsSyncer] result.code =', result.code)
+
     if syncer.on_after_sync ~= nil then
-        syncer.on_after_sync(syncer)
+        syncer.on_after_sync(syncer, result)
     end
 
     _tmr:start()
@@ -28,15 +37,22 @@ local syncer_sync = function(syncer, _tmr)
     print('[DynDnsSyncer] get_public_ip()...')
     
     get_public_ip(function(code, public_ip)
-        if(code < 0) then
+        if code ~= 200 then
             print('http req failed', code)
-            syncer_fire_after_sync(syncer, _tmr)
+            
+            syncer_fire_after_sync(syncer, {
+                code = NameCheapDynDnsSyncer.PUBLIC_IP_SERVICE_HTTP_REQ_ERROR,
+                http_code = code
+            }, _tmr)
         else
             print('[DynDnsSyncer] public_ip =', public_ip)
         
             if syncer.public_ip ~= nil and syncer.public_ip == public_ip then
                 print('[DynDnsSyncer] public ip has not changed')
-                syncer_fire_after_sync(syncer, _tmr)
+                
+                syncer_fire_after_sync(syncer, {
+                    code = NameCheapDynDnsSyncer.PUBLIC_IP_NO_CHANGE
+                }, _tmr)
                 return
             end
 
@@ -50,51 +66,56 @@ local syncer_sync = function(syncer, _tmr)
                 pass   = syncer.pass,
                 ip     = syncer.public_ip
             }, function(code, res)
-                if(code < 0) then
+                if code ~= 200 then
                     print('http req failed', code)
-                    syncer_fire_after_sync(syncer, _tmr)
+                    
+                    syncer_fire_after_sync(syncer, {
+                        code = NameCheapDynDnsSyncer.DNR_RECORD_UPDATE_SERVICE_HTTP_REQ_ERROR,
+                        http_code = code
+                    }, _tmr)
                 else
                     print('[DynDnsSyncer]', res)
-                    syncer_fire_after_sync(syncer, _tmr)
+                    
+                    syncer_fire_after_sync(syncer, {
+                        code = NameCheapDynDnsSyncer.DNS_RECORD_UPDATED
+                    }, _tmr)
                 end
             end)
         end
     end)
 end
 
-NameCheapDynDnsSyncer = {
-    create = function(conf)
-        local self = {
-            interval       = conf.interval,
-            host           = conf.host,
-            domain         = conf.domain,
-            pass           = conf.pass,
-            on_before_sync = conf.on_before_sync,
-            on_after_sync  = conf.on_after_sync,
-            global_ip      = nil
-        }
+NameCheapDynDnsSyncer.create = function(conf)
+    local self = {
+        interval       = conf.interval,
+        host           = conf.host,
+        domain         = conf.domain,
+        pass           = conf.pass,
+        on_before_sync = conf.on_before_sync,
+        on_after_sync  = conf.on_after_sync,
+        global_ip      = nil
+    }
 
-        local sync_tmr = tmr.create()
+    local sync_tmr = tmr.create()
 
-        sync_tmr:register(self.interval * 6e4, tmr.ALARM_AUTO, function()
-            syncer_sync(self, sync_tmr)
-        end)
+    sync_tmr:register(self.interval * 6e4, tmr.ALARM_AUTO, function()
+        syncer_sync(self, sync_tmr)
+    end)
 
-        self.fire_sta_connected = function(e, info) 
-            print('[STA ', e, '] to ', info.ssid)
-        end
-        
-        self.fire_sta_got_ip = function(e, info)
-            print('[STA ', e, '] ', info.ip, ' netmask = ', info.netmask, ' gateway = ', info.gw)
-
-            syncer_sync(self, sync_tmr)
-        end
-        
-        self.fire_sta_disconnected = function(e, info)
-            print('[STA ', e, '] reason = ', info.reason)
-            sync_tmr:stop()
-        end
-        
-        return self
+    self.fire_sta_connected = function(e, info) 
+        print('[DynDnsSyncer] STA:', e, 'to', info.ssid)
     end
-}
+    
+    self.fire_sta_got_ip = function(e, info)
+        print('[DynDnsSyncer] STA:', e, info.ip, ' netmask = ', info.netmask, ' gateway = ', info.gw)
+
+        syncer_sync(self, sync_tmr)
+    end
+    
+    self.fire_sta_disconnected = function(e, info)
+        print('[DynDnsSyncer] STA:', e, 'reason =', info.reason)
+        sync_tmr:stop()
+    end
+    
+    return self
+end
