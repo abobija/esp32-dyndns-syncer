aa89f16bcee9fd8b41d1fc7e4dc262b9297b7864
Alija Bobija <bobija.alija@gmail.com>
2019-03-14 12:17:13 +0100

    Buffering http request body
    Functionality to receiving and buffering packets that represent http request body

--- api.lua
+++ api.lua
@@ -1,5 +1,12 @@
 local Api = {}
 
+local function str_starts_with(haystack, needle)
+    local h_len = haystack:len()
+    local n_len = needle:len()
+
+    return n_len <= h_len and haystack:sub(1, n_len) == needle
+end
+
 local function str_split(inputstr, sep)
     if sep == nil then sep = "%s" end
     local result = {}
@@ -17,18 +24,39 @@ Api.json_stringify = function(table)
     return nil
 end
 
-Api.parse_http_request = function(request)
-    local first_rn = request:find("\r\n")
+local function get_http_header_value(hname, hlines)
+    for _, hline in pairs(hlines) do
+        if str_starts_with(hline, hname) then
+            local colon_index = hline:find(':')
 
-    if first_rn ~= nil then
-        local parts = str_split(request:sub(1, first_rn))
-    
-        if #parts == 3 then
-            return {
-                method = parts[1],
-                path   = parts[2],
-                std    = parts[3]
+            if colon_index ~= nil then
+                return hline:sub(colon_index + 2)
+            end
+        end
+    end
+
+    return nil
+end
+
+Api.parse_http_header = function(request)
+    local hlines = str_split(request, "\r\n")
+
+    if #hlines > 0 then
+        local hline1_parts = str_split(hlines[1])
+        
+        if #hline1_parts == 3 and hline1_parts[3] == 'HTTP/1.1' then
+            local result = {
+                method         = hline1_parts[1],
+                path           = hline1_parts[2],
+                std            = hline1_parts[3],
+                content_length = get_http_header_value('Content-Length', hlines)
             }
+
+            if result.content_length ~= nil then
+                result.content_length = tonumber(result.content_length)
+            end
+
+            return result
         end
     end
 
@@ -37,11 +65,13 @@ end
 
 Api.create = function(conf) 
     local self = {
-        port = conf.port
+        port          = conf.port,
+        http_body_min = 10,
+        http_body_max = 512
     }
 
     local endpoints = {}
-
+    
     self.add_endpoint = function(path, handler)
         table.insert(endpoints, {
             path    = path,
@@ -50,7 +80,7 @@ Api.create = function(conf)
         
         return self
     end
-
+    
     local get_endpoint_by_path = function(path)
         for _, ep in pairs(endpoints) do
             if ep.path == path then return ep end
@@ -61,54 +91,114 @@ Api.create = function(conf)
     
     local srv = net.createServer(net.TCP, 30)
 
-    local receiver = function(sck, data)
+    local sending     = false
+    local http_header = nil
+    local http_body   = nil
+    
+    local function stop_rec()
+        sending     = false
+        http_header = nil
+        http_body   = nil
+    end
+    
+    local function parse_http_request(sck)
         local res = {}
         
         local send = function(_sck)
+            sending = true
+            
             if #res > 0 then
                 _sck:send(table.remove(res, 1))
             else
+                sending = false
                 _sck:close()
-                response = nil
+                res = nil
             end
         end
         
-        local req = Api.parse_http_request(data)
-        data = nil
-
+        sck:on('sent', send)
+        
         local response_status = '200 OK'
         
         res[1] = 'HTTP/1.1 '
         res[#res + 1] = "Content-Type: application/json; charset=UTF-8\r\n"
         res[#res + 1] = "\r\n"
         
-        if req == nil then
+        if http_header == nil then
             response_status = '400 Bad Request'
         else
-            print('[DynDnsSyncer:API]', 'method:' .. req.method .. ', path:' .. req.path .. ', std:' .. req.std)
-            
-            local ep = get_endpoint_by_path(req.path)
+            local ep = get_endpoint_by_path(http_header.path)
             local res_json_tbl = {}
             
             if ep == nil then
                 response_status = '404 Not Found'
             else
-                res_json_tbl = ep.handler(self, req)
+                stop_rec()
+                
+                res_json_tbl = ep.handler(self, http_header)
                 res[#res + 1] = Api.json_stringify(res_json_tbl)
                 res_json_tbl = nil
             end
         end
-
+        
         res[1] = res[1] .. response_status .. "\r\n"
-        sck:on("sent", send)
+        
+        stop_rec()
         send(sck)
     end
+    
+    local on_receive = function(sck, data)
+        print(data)
+        if sending then return end
+        
+        if http_header == nil then
+            http_header = Api.parse_http_header(data)
+            data = nil
+            
+            if http_header ~= nil then
+                -- Received data that propbably represent http header
+                print('method ', http_header.method, ' path ', http_header.path, ' std ', http_header.std, ' content-length ', http_header.content_length)
 
-    if srv then
-        srv:listen(self.port, function(conn)
-            conn:on('receive', receiver)
-        end)
+                if http_header.content_length == nil
+                    or http_header.content_length < self.http_body_min
+                    or http_header.content_length > self.http_body_max then
+                    -- It seems like request body is too short, too big or does not exist at all.
+                    -- Parse request immediatelly
+                    
+                    parse_http_request(sck)
+                end
+            else
+                -- Received some data which is not represent http header.
+                -- Let's parse it anyway because error 400 shoud be sent to the client
+                
+                parse_http_request(sck)
+            end
+        else
+            -- Receiving body packets
+            
+            if http_body == nil then
+                http_body = data
+            else
+                http_body = http_body .. data
+            end
+
+            local http_body_len = http_body:len()
+            
+            if http_body_len >= http_header.content_length
+                or http_body_len >= self.http_body_max then
+                -- Received enough bytes of request body.
+                
+                parse_http_request(sck)
+            end
+        end
     end
+
+    srv:listen(self.port, function(conn)
+        stop_rec()
+    
+        conn:on('receive', on_receive)
+        conn:on('disconnection', stop_rec)
+    end)
 
     return self
 end
